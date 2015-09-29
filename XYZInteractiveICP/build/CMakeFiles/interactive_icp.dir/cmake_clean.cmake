c9b7c8193d5d8ce96a4e40ae88017b11131b3fef
mario2904 <orbegoso2904@gmail.com>
2015-09-29 10:22:11 -0400

    changed file structure

---
+++ XYZInteractiveICP/build/CMakeFiles/interactive_icp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/interactive_icp.dir/interactive_icp.cpp.o"
+  "interactive_icp.pdb"
+  "interactive_icp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/interactive_icp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
